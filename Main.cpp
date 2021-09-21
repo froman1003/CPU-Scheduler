fc519d63305678488d6cefa70b0cae4f8ffbaff8
froman1003 <61164317+froman1003@users.noreply.github.com>
2021-09-21 13:18:46 -0400

    Updated Main.cpp
    Made changes to fit changes in MultiQueue class.

--- Main.cpp
+++ Main.cpp
@@ -202,7 +202,8 @@ void MLFQ(MultiQueue& mq)
 				{
 					printf("(%s has moved to Queue %d!) ", returnedProcess.GetName(), queueNum + 1);
 					returnedProcess.SetDowngraded(false);
-					mq.Add(std::move(returnedProcess), queueNum);
+					//mq.Add(std::move(returnedProcess), queueNum);
+					mq.AddToNextQueue(std::move(returnedProcess));
 				}
 				else
 				{
@@ -231,7 +232,8 @@ void MLFQ(MultiQueue& mq)
 				{
 					process.NextBurst();
 					printf("(%s has left I/O Queue!) ", process.GetName());
-					mq.Add(std::move(process), queueNum - 1);
+					//mq.Add(std::move(process), queueNum - 1);
+					mq.Add(std::move(process));
 					ioIterator = ioList.erase(ioIterator);
 				}
 				else
@@ -310,28 +312,28 @@ int main()
 	MultiQueue mq;
 
 	bursts.assign({ 5, 27, 3, 31, 5, 43, 4, 18, 6, 22, 4, 26, 3, 24, 4 }); //225
-	mq.Add(std::move(Process(bursts)), 0);
+	mq.Add(std::move(Process(bursts)));
 
 	bursts.assign({ 4, 48, 5, 44, 7, 42, 12, 37, 9, 76, 4, 41, 9, 31, 7, 43, 8 }); //427
-	mq.Add(std::move(Process(bursts)), 0);
+	mq.Add(std::move(Process(bursts)));
 
 	bursts.assign({ 8, 33, 12, 41, 18, 65, 14, 21, 4, 61, 15, 18, 14, 26, 5, 31, 6 }); //392
-	mq.Add(std::move(Process(bursts)), 0);
+	mq.Add(std::move(Process(bursts)));
 
 	bursts.assign({ 3, 35, 4, 41, 5, 45, 3, 51, 4, 61, 5, 54, 6, 82, 5, 77, 3 }); //484
-	mq.Add(std::move(Process(bursts)), 0);
+	mq.Add(std::move(Process(bursts)));
 
 	bursts.assign({ 16, 24, 17, 21, 5, 36, 16, 26, 7, 31, 13, 28, 11, 21, 6, 13, 3, 11, 4 }); //309
-	mq.Add(std::move(Process(bursts)), 0);
+	mq.Add(std::move(Process(bursts)));
 
 	bursts.assign({ 11, 22, 4, 8, 5, 10, 6, 12, 7, 14, 9, 18, 12, 24, 15, 30, 8 }); //215
-	mq.Add(std::move(Process(bursts)), 0);
+	mq.Add(std::move(Process(bursts)));
 
 	bursts.assign({ 14, 46, 17, 41, 11, 42, 15, 21, 4, 32, 7, 19, 16, 33, 10 }); //328
-	mq.Add(std::move(Process(bursts)), 0);
+	mq.Add(std::move(Process(bursts)));
 
 	bursts.assign({ 4, 14, 5, 33, 6, 51, 14, 73, 16, 87, 6 }); //309
-	mq.Add(std::move(Process(bursts)), 0);
+	mq.Add(std::move(Process(bursts)));
 
 	MLFQ(mq);
 #pragma endregion
