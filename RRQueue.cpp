4cd68d27be90a103f6adae847703dbb5e704fc9d
froman1003 <froman1003@gmail.com>
2021-10-01 11:27:48 -0400

    Updated RRQueue Class
    Simplified RRQueue class.

--- RRQueue.cpp
+++ RRQueue.cpp
@@ -1,17 +1,11 @@
 #include "RRQueue.h"
 
 RRQueue::RRQueue(int timeQuantum)
-	: m_timeQuantum(timeQuantum) { }
+	: m_timeQuantum(timeQuantum), m_timeLeft(timeQuantum) { }
 
 bool RRQueue::Update(int runTime)
 {
-	static int timeLeft = m_timeQuantum; //determines whether or not CPU bursts are finished within the assigned time quantum.
-
-	if (m_bWasInterrupted)
-	{
-		timeLeft = m_timeQuantum;
-		m_bWasInterrupted = false;
-	}
+	//static int timeLeft = m_timeQuantum; //determines whether or not CPU bursts are finished within the assigned time quantum.
 
 	if (m_pFront == nullptr)
 	{
@@ -25,7 +19,7 @@ bool RRQueue::Update(int runTime)
 	//Check if current process' CPU burst is complete
 	if (pCurrentProcess->IsBurstFinished() == false)
 	{
-		if (timeLeft != 0 || m_pItrNode != m_pFront)
+		if (m_timeLeft != 0 || m_pItrNode != m_pFront)
 			pCurrentProcess->DisplayProgress();
 
 		//If process is not first in ready queue, increment its wait time.
@@ -33,20 +27,20 @@ bool RRQueue::Update(int runTime)
 		{
 			pCurrentProcess->Wait();
 		}
-		else if (timeLeft != 0) //If process is first in ready queue, burst, and decrement time it has left to complete CPU burst.
+		else if (m_timeLeft != 0) //If process is first in ready queue, burst, and decrement time it has left to complete CPU burst.
 		{
-			--timeLeft;
+			--m_timeLeft;
 			pCurrentProcess->Burst(runTime);
 		}
-		else if (timeLeft == 0)
+		else if (m_timeLeft == 0)
 		{
-			timeLeft = m_timeQuantum;
+			m_timeLeft = m_timeQuantum;
 			returnValue = true;
 		}
 	}
 	else //If CPU burst is complete, notify update loop (in Main.cpp) that the current process will be transferred to I/O list.
 	{
-		timeLeft = m_timeQuantum;
+		m_timeLeft = m_timeQuantum;
 		returnValue = true;
 	}
 
@@ -54,4 +48,10 @@ bool RRQueue::Update(int runTime)
 	IncrementIteratorNode();
 
 	return returnValue;
+}
+
+void RRQueue::ResetTimeLeft()
+{
+	m_timeLeft = m_timeQuantum;
+	//m_bWasInterrupted = true;
 }
--- RRQueue.h
+++ RRQueue.h
@@ -6,11 +6,13 @@ class RRQueue : public ReadyQueue
 
 private:
 	unsigned int m_timeQuantum;
+	unsigned int m_timeLeft;
 
 public:
 	RRQueue(int timeQuantum);
 
 	virtual bool Update(int runTime) override;
+	virtual void ResetTimeLeft() override;
 	
 };
 
